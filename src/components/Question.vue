c6273ed595542985e6f579ee6241f62d660628bd
swanndolia-capgemini <thomas.lannoy@capgemini.com>
2021-12-17 10:00:10 +0100

    first push

---
+++ src/components/Question.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="question">
+    {{ question.question }}<br /><br />
+    <span v-for="answer in question.answers" :key="answer">
+      <label :id="answer"
+        ><input
+          type="checkbox"
+          :value="answer"
+          v-model="checkedAnswer"
+          @change="check"
+        />{{ answer }}</label
+      >
+      <br />
+    </span>
+    <button @click="checkAnswer(question.correct)">Valider</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      checkedAnswer: [],
+    };
+  },
+  methods: {
+    checkAnswer(correct) {
+      for (let element in this.checkedAnswer) {
+        if (!correct.includes(this.checkedAnswer[element])) {
+          document.getElementById(this.checkedAnswer[element]).style.color = "red";
+        }
+        else{
+          document.getElementById(this.checkedAnswer[element]).style.color = "green";
+        }
+      }
+      for (let element in correct) {
+        if (!this.checkedAnswer.includes(correct[element])) {
+          document.getElementById(correct[element]).style.color = "green";
+        }
+      }
+    },
+  },
+  props: {
+    question: Object,
+  },
+};
+</script>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <ul class="question-box" v-for="question in questions" :key="question">
+      <li v-for="name in question" :key="name">
+        <Question :question="name" />
+      </li>
+      <br />
+    </ul>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Question from "@/components/Question.vue";
+import questionList from "../questions.json";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      questions: questionList,
+    };
+  },
+  components: {
+    Question,
+  },
+};
+</script>
+<style lang="scss" scoped="true">
+.question-box {
+  background: lightgray;
+  border-radius: 20px;
+  margin: 15px;
+  padding: 30px;
+}
+</style>
